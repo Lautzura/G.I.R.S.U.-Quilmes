0a18992e30fb7f71ac3f8d931267989dae740368
Lautzura <leivalautaro316@gmail.com>
2026-01-20 16:25:02 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,18 +1,15 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { dataService } from './services/api';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
-// Ya no creamos el servicio aquí, lo importamos del singleton global
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App dataService={dataService} />
+    <App />
   </React.StrictMode>
 );
